5695f35f7ea19f7845e3dcb0ffdda52df5d2259a
robinviltoriano <robin.vito3@gmail.com>
2025-09-26 19:42:11 +0800

    update time variable

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def index():
 
     # Query the data from the table
     data = YourTable.query.all()
-    my_date = data[0].time
     news_dictionary = [{"headline": item.headline, "img": item.img,"url": item.url,"summarized_text": item.summarized_text} for item in data]
+    my_date = news_dictionary[0].get('time') if news_dictionary else None
     return render_template('index.html', my_date = my_date, news_dictionary = news_dictionary)
 
 if __name__=='__main__':
